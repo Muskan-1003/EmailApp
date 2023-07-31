3bb59446e8991bfa0a0e8daaaafa123faa129619
Muskan Pandey <muskanpandey1003@gmail.com>
2023-07-31 14:10:29 +0530

    Add files via upload

---
+++ EmailApp/src/emailapp/Email.java
@@ -0,0 +1,23 @@
+package emailapp;
+
+public class Email {//objects
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor to receive first and last name
+
+    //Ask for department
+
+    //generate random password
+
+    //set the mailbox capacity
+
+    //set the alternate email
+
+    //change the password
+
+}
